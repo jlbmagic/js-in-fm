153009daaffdb84308cdc4a94fb91114496a23cc
Jeremy Brown <jeremy.brown@proofgeist.com>
2021-09-13 12:58:55 -0400

    removed template stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,14 +13,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
@@ -35,7 +27,7 @@ export default function Home() {
     >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <h1>This is the detail about JS in FM</h1>
       </main>
     </Layout>
   );
